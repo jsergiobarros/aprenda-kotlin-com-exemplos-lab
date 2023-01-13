72eb964a5b6ac9e89d8253c7dbc825c773b98111
Sérgio Barros <32466151+jsergiobarros@users.noreply.github.com>
2023-01-12 21:58:49 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class ConteudoEducacional(val nome: String, val nivel: Nivel ,val duracao: Int = 60){
+    
+}
 
-class Usuario
+class Usuario(val nome: String){
+    
+    val conteudos = mutableListOf<ConteudoEducacional>()
+    
+    fun concluido(conteudo:ConteudoEducacional){
+       this.conteudos.add(conteudo)
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        this.inscritos.add(usuario)
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    var bootcamp = Formacao("WEB", Nivel.INTERMEDIARIO, listOf(ConteudoEducacional("javascript",Nivel.INTERMEDIARIO),ConteudoEducacional("HTML",Nivel.INICIANTE)))
+    
+	bootcamp.matricular(Usuario("Cleuton Ribeiro"))
+    bootcamp.matricular(Usuario("Cilene Ribeiro"))
+    
 }
